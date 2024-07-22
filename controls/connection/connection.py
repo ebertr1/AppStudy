0e64d14d404e25dbdcd30798a1b265a28b5802da
Eber_tr1 <eberson.guayllas@unl.edu.ec>
2024-07-22 00:37:56 -0500

    cambios

--- controls/connection/connection.py
+++ controls/connection/connection.py
@@ -1,4 +1,4 @@
-import oracledb
+import oracledb  # Asegúrate de tener la biblioteca oracledb instalada
 
 class Connection:
     def __init__(self):
@@ -15,18 +15,19 @@ class Connection:
     def connect(self):
         try:
             self.__db = oracledb.connect(
-                user='user_developer',
-                password='123456',
+                user='user_pis',
+                password='eber2005',
                 dsn='localhost:1521/xe'
             )
         except Exception as error:
             print('Error de conexión: ', error)
             exit()
         else:
-            print('Conexión exitosa', self.__db.version)
+            # Imprime un mensaje de conexión exitosa y la versión de la base de datos
+            print('Conexión exitosa. Versión de la base de datos:', self.__db.version)
         return self
 
     def close(self):
         if self.__db:
             self.__db.close()
-
+            print('Conexión cerrada.')
